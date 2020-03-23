43538e5c1bce87bc56ea0d935759a0340059b75c
Sławomir Rembiesa <slawomir.rembiesa@identitybank.eu>
2020-03-23 10:36:32 +0100

    jira:IDB-1304 back-end forms

--- modules/formsTemplate/controllers/FormsTemplateController.php
+++ modules/formsTemplate/controllers/FormsTemplateController.php
@@ -35,6 +35,13 @@ namespace app\modules\formsTemplate\controllers;
 ################################################################################
 
 use app\controllers\IdbController;
+use app\helpers\Translate;
+use idbyii2\components\PortalApi;
+use idbyii2\helpers\IdbAccountId;
+use idbyii2\models\db\FormTemplate;
+use idbyii2\models\form\FTemplateForm;
+use idbyii2\models\idb\IdbBankClientBusiness;
+use Yii;
 use yii\helpers\ArrayHelper;
 
 ################################################################################
@@ -48,13 +55,25 @@ use yii\helpers\ArrayHelper;
 class FormsTemplateController extends IdbController
 {
 
+    /**
+     * @var array
+     */
     private static $params = [
         'menu_active_section' => '[menu][template]',
         'menu_active_item' => '[menu][template][editor]'
     ];
 
+    private $version = '1.0';
+
+    /**
+     * @return string
+     * @throws \Exception
+     */
     public function actionIndex()
     {
+        $model = new FTemplateForm();
+        $usersToSend = $this->getUsersToSend();
+
         return $this->render(
             '@app/themes/adminlte2/views/site/template',
             [
@@ -62,10 +81,111 @@ class FormsTemplateController extends IdbController
                 (
                     self::$params,
                     [
-                        'content' => 'index'
+                        'content' => 'index',
+                        'contentParams' => [
+                            'model' => $model,
+                            'usersToSend' => $usersToSend
+                        ]
                     ]
                 )
             ]
         );
+    }
+
+    /**
+     * save data to Database
+     * @throws \Exception
+     */
+    public function actionSave()
+    {
+        $dataToJson = [];
+        $form = Yii::$app->getRequest()->post()['FTemplateForm']['data'];
+        $peopleUser = Yii::$app->getRequest()->post()['peopleUser'];
+        $userID = Yii::$app->user->identity->getId();
+
+        for ($i = 0; $i < count($form['fieldName']); $i++) {
+            if (($form['fieldName'][$i] == '') && ($form['fieldDesc'][$i] == 0)) {
+                continue;
+            } else {
+                $fieldName = $form['fieldName'][$i];
+                $type = $form['type'][$i];
+                $fieldDesc = $form['fieldDesc'][$i];
+
+                $arr = [
+                    'fieldName' => $fieldName,
+                    'type' => $type,
+                    'fieldDesc' => $fieldDesc
+                ];
+
+                $dataToJson[] = $arr;
+            }
+        }
+
+        $json = json_encode($dataToJson);
+
+        try {
+            $model = new FormTemplate();
+            $model->business_user = $userID;
+            $model->people_user = $peopleUser;
+            $model->version_editor = $this->version;
+            $model->data_json = $json;
+            $model->save();
+
+            $flash = [
+                'subject' => Translate::_('business', 'success'),
+                'message' => Translate::_(
+                    'business',
+                    'Form saved and sent correctly.'
+                )
+            ];
+
+            Yii::$app->session->setFlash('success', $flash);
+
+        } catch (\Exception $e) {
+            $flash = [
+                'subject' => Translate::_('business', 'error'),
+                'message' => Translate::_(
+                    'business',
+                    'Something went wrong. Try again..'
+                )
+            ];
+
+            Yii::$app->session->setFlash('error', $flash);
+        }
+
+        $this->redirect(['index']);
+
+    }
+    
+    /**
+     * @return array
+     * @throws \Exception
+     */
+    private function getUsersToSend()
+    {
+        $isEmpty = false;
+
+        $user = Yii::$app->user->identity;
+        $businessId = IdbAccountId::generateBusinessDbUserId($user->oid, $user->aid, $user->dbid, '%');
+        $clientModel = IdbBankClientBusiness::model($businessId);
+        $relations = $clientModel->getRelatedPeoples($businessId);
+        if (empty($relations['QueryData'])) {
+            $usersName = [];
+            $isEmpty = true;
+        } else {
+            $portalPeopleApi = PortalApi::getPeopleApi();
+            $usersName = $portalPeopleApi->requestPeopleInfo($relations['QueryData']);
+        }
+
+        $userToSend = [];
+
+        if ($isEmpty == false) {
+            foreach ($usersName as $user) {
+                $name = $user['name'] . $user['surname'];
+                $userToSend[$user['userId']] = $name;
+            }
+        }
+
+        return $userToSend;
     }
 }
--- themes/adminlte2/modules/formsTemplate/views/forms-template/index.php
+++ themes/adminlte2/modules/formsTemplate/views/forms-template/index.php
@@ -2,7 +2,10 @@
 
 use app\assets\FrontAsset;
 use app\helpers\Translate;
+use yii\bootstrap\ActiveForm;
 use yii\helpers\Html;
+use yii\helpers\Url;
+use idbyii2\widgets\FlashMessage;
 
 $frontAssets = FrontAsset::register($this);
 $frontAssets->loadEditor();
@@ -22,37 +25,96 @@ $frontAssets->loadEditor();
             <div class="col-xs-12">
                 <div class="box">
                     <div class="box-body">
+
+                        <?= FlashMessage::widget(
+                            [
+                                'success' => Yii::$app->session->hasFlash('success')
+                                    ? Yii::$app->session->getFlash(
+                                        'success'
+                                    ) : null,
+                                'error' => Yii::$app->session->hasFlash('error') ? Yii::$app->session->getFlash(
+                                    'error'
+                                )
+                                    : null,
+                                'info' => Yii::$app->session->hasFlash('info') ? Yii::$app->session->getFlash(
+                                    'info'
+                                )
+                                    : null,
+                            ]
+                        ); ?>
+                        <?php if(!empty($usersToSend)): ?>
                         <div id="editor">
-                            <table style="width:100%" class="table table-bordered">
-                                <tr>
-                                    <th><?= Translate::_('business', 'Field name') ?></th>
-                                    <th><?= Translate::_('business', 'Type') ?></th>
-                                    <th><?= Translate::_('business', 'Description') ?></th>
-                                    <th><?= Translate::_('business', 'Remove') ?></th>
-                                </tr>
-                                <tr v-for="(row, index) in rows">
-                                    <td><input v-bind:style="inputStyle" type="text" v-model="row.name"></td>
-                                    <td>
-                                        <select v-bind:style="inputStyle">
-                                            <option v-for="type in types" v-bind:value="type.value">
-                                                {{ type.value }}
-                                            </option>
-                                        </select>
-                                    </td>
-                                    <td><input v-bind:style="inputStyle" type="text" v-model="row.description"></td>
-                                    <td v-bind:style="centerBtnRow">
-                                        <a v-on:click="removeElement(index);" class="btn btn-app-trash"
-                                           style="cursor: pointer"><i class="glyphicon glyphicon-trash"></i></a>
-                                    </td>
-                                </tr>
-                            </table>
-                            <div v-bind:style="centerBtnRow">
-                                <button class="btn btn-primary" @click="addRow">Add row</button>
+                            <?php $form = ActiveForm::begin(
+                                [
+                                    'action' => Url::toRoute(['/forms-template/forms-template/save'], true),
+                                    'options' => ['method' => 'post']
+                                ]
+                            ); ?>
+                            <div class="form-group">
+
+                                <?= $form->field($model, 'peopleUser')->dropDownList(
+                                    $usersToSend,
+                                    [
+                                        'name' => 'peopleUser',
+                                    ]
+                                ) ?>
+                                <table style="width:100%" class="table table-bordered">
+                                    <tr>
+                                        <th><?= Translate::_('business', 'Field name') ?></th>
+                                        <th><?= Translate::_('business', 'Type') ?></th>
+                                        <th><?= Translate::_('business', 'Description') ?></th>
+                                        <th><?= Translate::_('business', 'Remove') ?></th>
+                                    </tr>
+                                    <tr v-for="(row, index) in rows">
+                                        <td><?= $form->field($model, 'data[fieldName][]')->textInput(
+                                                ['v-model' => 'row.name', 'v-bind:style' => 'inputStyle']
+                                            )->label(false) ?></td>
+                                        <td>
+                                            <select v-bind:style="inputStyle" name="FTemplateForm[data][type][]">
+                                                <option v-for="type in types" v-bind:value="type.value">
+                                                    {{ type.value }}
+                                                </option>
+                                            </select>
+                                        </td>
+                                        <td><?= $form->field($model, 'data[fieldDesc][]')->textInput(
+                                                ['v-model' => 'row.description', 'v-bind:style' => 'inputStyle']
+                                            )->label(false) ?></td>
+                                        <td v-bind:style="centerBtnRow">
+                                            <a v-on:click="removeElement(index);" class="btn btn-app-trash"
+                                               style="cursor: pointer"><i class="glyphicon glyphicon-trash"></i></a>
+                                        </td>
+                                    </tr>
+                                </table>
+                                <div v-bind:style="centerBtnRow">
+                                    <?= Html::button(
+                                        'Add row',
+                                        ['class' => 'btn btn-primary', 'v-on:click' => 'addRow']
+                                    ) ?>
+                                </div>
+                                <hr>
+                                <div id="submit">
+                                    <?= Html::submitButton(
+                                        Translate::_('business', 'Save'),
+                                        ['class' => 'btn btn-primary', 'style' => 'float: right;']
+                                    ) ?>
+                                </div>
                             </div>
+                            <?php ActiveForm::end(); ?>
+                            <?php else: ?>
+                                <div class="modal-no-user">
+                                    <div class="alert alert-danger" role="alert">
+                                        <?= Translate::_(
+                                            'business',
+                                            'There are no personal account holders connected to this business yet.'
+                                        ) ?>
+                                    </div>
+                                </div>
+                            <?php endif; ?>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </section>
+
 </div>
